0267e2a5bcfc9770cf067f153fc40f7e55831685
TgDgNU <tgdgnu@gmail.com>
2018-03-13 19:39:01 +0300

    upd

--- function.findEnergyFromMemory.js
+++ function.findEnergyFromMemory.js
@@ -15,7 +15,10 @@ var findEnergyFromMemory = {
         if (temp.length>0) {
             //[closestSourceId,closestSourceIdType]=creep.pos.findClosestByPath(temp.map( roomEnergyObject=> [Game.getObjectById(roomEnergyObject[0]),Game.getObjectById(roomEnergyObject[2])])).id;
             closestSourceId=creep.pos.findClosestByPath(temp.map( roomEnergyObject=> Game.getObjectById(roomEnergyObject[2]))).id;
-            console.log(closestSourceId);
+            console.log("####")
+            console.log("found source id "+closestSourceId);
+            console.log(Memory.rooms[creep.room.name]["roomEnergyArray"])
+            console.log("####")
             console.log(_.findIndex(Memory.rooms[creep.room.name]["roomEnergyArray"],function(roomEnergyArrayItem) {roomEnergyArrayItem[2]==closestSourceId}))
             closestSourceIdType=Memory.rooms[creep.room.name]["roomEnergyArray"][_.findIndex(Memory.rooms[creep.room.name]["roomEnergyArray"],function(roomEnergyArrayItem) {roomEnergyArrayItem[2]==closestSourceId})]
             //closestSourceIdType="temp"

c82168f1cb59b817244223d411176bec6bba50bf
TgDgNU <tgdgnu@gmail.com>
2018-03-13 18:50:29 +0300

    upd_check_findEnergyFM
    added temp

--- function.findEnergyFromMemory.js
+++ function.findEnergyFromMemory.js
@@ -3,15 +3,14 @@ var lib=require('function.libraries');
 var findEnergyFromMemory = {
     run: function findEnergyFromMemory(creep) {
 
-        if (!Memory.room[creep.room.name]["roomEnergyArray"] || Memory.room[creep.room.name]["roomEnergyArray"].length==0) {
+        if (!Memory.rooms[creep.room.name]["roomEnergyArray"] || Memory.rooms[creep.room.name]["roomEnergyArray"].length==0) {
             return (false)
         }
 
+        temp=_.filter(Memory.rooms[creep.room.name]["roomEnergyArray"],function(roomEnergyObject) { return roomEnergyObject[1]>=creep.carryCapacity})
 
-        [closestSourceId,closestSourceIdType]=creep.pos.findClosestByPath(_.filter(Memory.rooms[creep.room.name]["roomEnergyArray"],
-            function(roomEnergyObject) { return roomEnergyObject[1]>=creep.carryCapacity}).map( roomEnergyObject=> [Game.getObjectById(roomEnergyObject[0]),Game.getObjectById(roomEnergyObject[2])])).id;
-
-        if (closestSourceId) {
+        if temp.length>0 {
+            [closestSourceId,closestSourceIdType]=creep.pos.findClosestByPath(temp.map( roomEnergyObject=> [Game.getObjectById(roomEnergyObject[0]),Game.getObjectById(roomEnergyObject[2])])).id;
             return [closestSourceId,closestSourceIdType]
         }
         else {
